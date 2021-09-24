91bc81ceaff89f64e6a6cd408239405983fea830
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-09-24 15:33:36 +0300

    add agreement page

--- js/login.js
+++ js/login.js
@@ -1,16 +1,26 @@
 jQuery(function ($) {
     $(document).ready(function () {
-        let loginForm = $('.login-form').find('form');
+        let loginForm = $('.send-form').find('form');
         loginForm.submit(function (e) {
             var erroreArrayElemnts = [];
             var el = loginForm.find('[data-reqired]');
             for (let i = 0; i < el.length; i++) {
-                if (el[i].value === '' || el[i].value === ' ' || el[i].value === '-') {
-                    erroreArrayElemnts.push(el[i]);
-                    $(el[i]).parents('.login-input').addClass('has-error');
-                    $(el[i]).focus(function (e) {
-                        $(el[i]).parents('.login-input').removeClass('has-error');
-                    });
+                if(el[i].type === 'checkbox'){
+                    if(el[i].checked == false){
+                        erroreArrayElemnts.push(el[i]);
+                        $(el[i]).parents('.radio-input').addClass('has-error');
+                        $(el[i]).change(function (e) {
+                            $(el[i]).parents('.radio-input').removeClass('has-error');
+                        });
+                    }
+                } else {
+                    if (el[i].value === '' || el[i].value === ' ' || el[i].value === '-') {
+                        erroreArrayElemnts.push(el[i]);
+                        $(el[i]).parents('.login-input').addClass('has-error');
+                        $(el[i]).focus(function (e) {
+                            $(el[i]).parents('.login-input').removeClass('has-error');
+                        });
+                    }
                 }
             }
             if(erroreArrayElemnts.length > 0){
